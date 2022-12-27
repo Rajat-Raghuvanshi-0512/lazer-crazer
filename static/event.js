4efa4526408222383330c8b35908d1e4129b93af
pixysq <pixysq@gmail.com>
2022-12-28 03:46:39 +0530

    Booking api integrated

--- static/event.js
+++ static/event.js
@@ -50,3 +50,41 @@ window.addEventListener('scroll', function () {
     eventParallax10.style.top = value * 0.09 + 'px';
     eventParallax11.style.top = value * 0.09 + 'px';
 });
+
+const contactForm = document.querySelector("#contactForm");
+let name = document.getElementById("name");
+let phone = document.getElementById("phone");
+let email = document.getElementById("email");
+let select1 = document.getElementById("select1");
+let message = document.getElementById("message");
+let submitButton = document.querySelector("#sendRequest");
+
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let formData = {
+        name: name.value,
+        phone: phone.value,
+        email: email.value,
+        select1: select1.value,
+        message: message.value,
+    }
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.send(JSON.stringify(formData));
+    xhr.onload = function () {
+        console.log(xhr.responseText);
+
+        if (xhr.responseText == 'success') {
+            name.value = '';
+            email.value = '';
+            phone.value = '';
+            select1.value = '';
+            message.value = '';
+        } else {
+            alert('Something went wrong!')
+        }
+    }
+});
