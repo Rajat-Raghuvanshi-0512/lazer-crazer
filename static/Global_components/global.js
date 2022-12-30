57686cb9a9df19ea2a9b69501ef5dc3b88fb74a2
pixysq <pixysq@gmail.com>
2022-12-30 17:12:05 +0530

    synced

--- static/Global_components/global.js
+++ static/Global_components/global.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const counttag = document.getElementsByClassName('middlesection')[0];
 const leftsub = document.getElementsByClassName('leftadder')[0];
 const rightadd = document.getElementsByClassName('rightadder')[0];
@@ -11,26 +10,11 @@ let time = "";
 console.log(slottag)
 let count = 1;
 counttag.innerHTML = `${count}`;
-=======
-const counttag=document.getElementsByClassName('middlesection')[0];
-const leftsub=document.getElementsByClassName('leftadder')[0];
-const rightadd=document.getElementsByClassName('rightadder')[0];
-const slottag=document.querySelector('.slotlist');
-
-
-
-let time="";
-
-console.log(slottag)
-let count=1;
-counttag.innerHTML=`${count}`;
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
 
 
 
 
 
-<<<<<<< HEAD
 
 
 const slots = document.getElementsByClassName('slots');
@@ -50,28 +34,6 @@ slottag.addEventListener('click', (event) => {
             }
             else {
                 element.style.borderColor = 'white'
-=======
-let flag=0;
-
-const slots=document.getElementsByClassName('slots');
-console.log(slots);
-slottag.addEventListener('click',(event)=>{
-    currenttarget=event.target;
-    console.log(currenttarget.classList);
-    if(currenttarget.classList.contains('slots')){
-        flag=1;
-        currenttarget.style.borderColor='#2CF80B'
-        currenttarget.style.boxShadow='0 0 20px #2CF70B'
-        time=currenttarget.innerHTML;
-        
-        for (let i = 0; i < slots.length; i++) {
-            const element= slots[i];
-            if(element===currenttarget){
-                continue;
-            }
-            else{
-                element.style.borderColor='white'
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
                 element.style.removeProperty('box-shadow')
             }
         }
@@ -81,7 +43,6 @@ slottag.addEventListener('click',(event)=>{
 
 //calender part
 const daysTag = document.querySelector(".days"),
-<<<<<<< HEAD
     currentDate = document.querySelector(".current-date"),
     prevNextIcon = document.querySelectorAll(".icons span");
 
@@ -99,25 +60,6 @@ const renderCalendar = () => {
         lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
         lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
         lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
-=======
-currentDate = document.querySelector(".current-date"),
-prevNextIcon = document.querySelectorAll(".icons span");
-
-// getting new date, current year and month
-let date = new Date(),
-currYear = date.getFullYear(),
-currMonth = date.getMonth();
-
-// storing full name of all months in array
-const months = ["January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"];
-
-const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
-    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
     let liTag = "";
 
     for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
@@ -126,13 +68,8 @@ const renderCalendar = () => {
 
     for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
         // adding active class to li if the current day, month, and year matched
-<<<<<<< HEAD
         let isToday = i === date.getDate() && currMonth === new Date().getMonth()
             && currYear === new Date().getFullYear() ? "active" : "";
-=======
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
-                     && currYear === new Date().getFullYear() ? "active" : "";
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
         liTag += `<li class="${isToday}">${i}</li>`;
     }
 
@@ -144,47 +81,12 @@ const renderCalendar = () => {
 }
 renderCalendar();
 
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-const todaysmonthyear=document.getElementsByClassName('current-date')[0];
-const changeddate=todaysmonthyear.innerHTML;
-let givenmonth=date.getMonth();
-let y= changeddate.substring(0,changeddate.length-5)
-let z= changeddate.substring(changeddate.length-4);
-for (let i = 0; i < 12; i++) {
-    const element = months[i];
-    if(element==y){
-        givenmonth=i;
-    }
-}
-
-
-let selecteddate=date.getDate();
-let selectedmonth=givenmonth;
-let selectedyear=z;
-
-
-
-
-
-
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
 prevNextIcon.forEach(icon => { // getting prev and next icons
     icon.addEventListener("click", () => { // adding click event on both icons
         // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
         currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
 
-<<<<<<< HEAD
         if (currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
-=======
-        if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
             // creating a new date of current year & month and pass it as date value
             date = new Date(currYear, currMonth);
             currYear = date.getFullYear(); // updating current year with new date year
@@ -193,32 +95,11 @@ prevNextIcon.forEach(icon => { // getting prev and next icons
             date = new Date(); // pass the current date as date value
         }
         renderCalendar(); // calling renderCalendar function
-<<<<<<< HEAD
-=======
-        const todaysmonthyear=document.getElementsByClassName('current-date')[0];
-        const changeddate=todaysmonthyear.innerHTML;
-        let givenmonth=date.getMonth();
-        let y= changeddate.substring(0,changeddate.length-5)
-        let z= changeddate.substring(changeddate.length-4);
-        for (let i = 0; i < 12; i++) {
-            const element = months[i];
-            if(element==y){
-                givenmonth=i;
-            }
-        }
-
-
-         selecteddate=date.getDate();
-         selectedmonth=givenmonth;
-         selectedyear=z;
-
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
     });
 });
 
 
 
-<<<<<<< HEAD
 let selecteddate = date.getDate();
 let selectedmonth = date.getMonth();
 let selectedyear = date.getFullYear();
@@ -269,56 +150,6 @@ const fetchslots = () => {
 
 
     })
-=======
-
-
-
-const fetchslots=()=>{
-    let todaysdate =selectedyear+'/'+(selectedmonth+1)+'/'+selecteddate;
-        console.log(todaysdate)
-
-
-        //fetching the API 
-        let fetchedslots=[];
-
-        const api_url ="http://localhost:5000/api/date/getslots/";
-  
-            // Defining async function
-            fetch(api_url, {
-                method: 'POST',
-                body: JSON.stringify({
-                    slotdate:todaysdate,
-                }),
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8'
-                }
-            }).then(function (response) {
-                if (response.ok) {
-                    return response.json();
-                }
-            }).then(function (data) {
-                console.log(data);
-                fetchedslots=data.slots
-            }).then(function(){
-                
-
-                const slotlist=document.getElementsByClassName('slotlist')[0];
-
-                let slots="";
-                for (let i = 0; i < fetchedslots.length; i++) {
-                    const element = fetchedslots[i];
-                    if(element.slotcount>=count){
-                        slots+=`<div class="slots">${element.slottime}</div>`;
-                    }
-                    
-                }
-
-
-                slotlist.innerHTML=slots
-
-
-            })
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
 
 }
 
@@ -328,7 +159,6 @@ fetchslots();
 
 
 
-<<<<<<< HEAD
 const linkele = document.querySelectorAll(".days li")
 // console.log(linkele)
 
@@ -338,38 +168,14 @@ daysTag.addEventListener("click", (event) => {
     if (currenttarget.tagName === 'LI') {
         currenttarget.classList.add('active');
         selecteddate = currenttarget.innerHTML;
-=======
-//calender date elements link  element was there
-
-
-
-// console.log(linkele)
-
-daysTag.addEventListener("click",(event)=>{
-    currenttarget=event.target;
-    console.log(currenttarget.tagName)
-const linkele=document.querySelectorAll(".days li")
-
-    if(currenttarget.tagName==='LI'){
-        flag=0;
-        currenttarget.classList.add('active');
-        selecteddate=currenttarget.innerHTML;
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
         console.log(selecteddate)
 
         for (let i = 0; i < linkele.length; i++) {
             const element = linkele[i];
-<<<<<<< HEAD
             if (element === currenttarget) {
                 continue;
             }
             else {
-=======
-            if(element===currenttarget){
-                continue;
-            }
-            else{
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
                 element.classList.remove('active');
             }
         }
@@ -377,7 +183,6 @@ const linkele=document.querySelectorAll(".days li")
 
 
         fetchslots();
-<<<<<<< HEAD
 
 
 
@@ -404,44 +209,12 @@ submitbutton.addEventListener('click', (event) => {
     popupsection.style.zIndex = '1';
 
 
-=======
-            
-            
-
-        }
-       
-
-    }
-)
-
-
-const submitbutton=document.getElementsByClassName('submitsection')[0];
-submitbutton.addEventListener('click',(event)=>{
-    if(flag){
-        let today =selectedyear+'/'+(selectedmonth+1)+'/'+selecteddate;
-    console.log(today)
-    console.log(count)
-    console.log(time)
-    const bookingsection=document.getElementsByClassName('bookingsection')[0];
-    bookingsection.classList.add('blurback')
-    const popupsection=document.getElementsByClassName('popupsubsection')[0];
-    popupsection.style.top='50%';
-    popupsection.style.visibility='visible';
-    popupsection.style.opacity='1';
-    popupsection.style.transition='0.5s';
-    popupsection.style.zIndex='1';
-    }
-    
-
-   
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
 
 });
 
 
 
 
-<<<<<<< HEAD
 rightadd.addEventListener("click", (event) => {
 
     count++;
@@ -459,31 +232,11 @@ leftsub.addEventListener("click", (event) => {
     }
     fetchslots();
 
-=======
-rightadd.addEventListener("click",(event)=>{
-    
-    count++;
-    if(count==16){
-        count=15
-    }
-    counttag.innerHTML=`${count}`;
-    fetchslots();     
-});
-leftsub.addEventListener("click",(event)=>{
-    counttag.innerHTML=`${count}`;
-    count--;
-    if(count==0){
-        count=1
-    }
-    fetchslots();        
-        
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
 
 });
 
 
 //popup cancel Javascript
-<<<<<<< HEAD
 const cancelbutton = document.getElementById('cancel');
 cancelbutton.addEventListener('click', (event) => {
     const bookingsection = document.getElementsByClassName('bookingsection')[0];
@@ -497,21 +250,6 @@ cancelbutton.addEventListener('click', (event) => {
 
 
 
-=======
-const cancelbutton=document.getElementById('cancel');
-cancelbutton.addEventListener('click',(event)=>{
-    const bookingsection=document.getElementsByClassName('bookingsection')[0];
-    bookingsection.classList.remove('blurback')
-    const popupsection=document.getElementsByClassName('popupsubsection')[0];
-    popupsection.style.top='40%';
-    popupsection.style.visibility='hidden';
-    popupsection.style.opacity='0';
-    popupsection.style.transition='0.5s';
-    popupsection.style.zIndex='-1';
-    
-    
-   
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
 });
 
 
@@ -519,7 +257,6 @@ cancelbutton.addEventListener('click',(event)=>{
 //final submit option
 
 
-<<<<<<< HEAD
 const finalsubmitbutton = document.querySelectorAll('.popupsubmit input')[0];
 
 finalsubmitbutton.addEventListener('click', (event) => {
@@ -532,20 +269,6 @@ finalsubmitbutton.addEventListener('click', (event) => {
 
     let todaysdate = selectedyear + '/' + (selectedmonth + 1) + '/' + selecteddate;
     console.log(todaysdate)
-=======
-const finalsubmitbutton=document.querySelectorAll('.popupsubmit input')[0];
-
-finalsubmitbutton.addEventListener('click',(event)=>{
-    const arr=document.querySelectorAll(".popupsections input");
-    const arrp=document.querySelectorAll(".popupsections select");
-    let clientname=arr[0].value;
-    let clientphoneno=arr[1].value;
-    let clientemail=arr[2].value;
-    let package=arrp[0].value
-
-    let todaysdate =selectedyear+'/'+(selectedmonth+1)+'/'+selecteddate;
-        console.log(todaysdate)
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
 
 
 
@@ -571,28 +294,12 @@ finalsubmitbutton.addEventListener('click',(event)=>{
 
 
 
-<<<<<<< HEAD
     const api_url = "https://www.lazercrazer.in/api/booking/addbooking";
 
-=======
-
-
-
-
-
-
-
-
-
-
-    const api_url ="http://localhost:5000/api/booking/addbooking";
-  
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
     // Defining async function
     fetch(api_url, {
         method: 'POST',
         body: JSON.stringify({
-<<<<<<< HEAD
             name: clientname,
             email: clientemail,
             phoneno: clientphoneno,
@@ -600,15 +307,6 @@ finalsubmitbutton.addEventListener('click',(event)=>{
             slotcount: count,
             slot: time,
             package: package
-=======
-        name:clientname,
-        email:clientemail,
-        phoneno:clientphoneno,
-        date:todaysdate,
-        slotcount:count,
-        slot:time,
-        package:package
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
         }),
         headers: {
             'Content-type': 'application/json; charset=UTF-8'
@@ -625,7 +323,6 @@ finalsubmitbutton.addEventListener('click',(event)=>{
 
         // paste SMTP request here
 
-<<<<<<< HEAD
         const contactFormB = document.querySelector("#contactFormB");
         let name = document.getElementById("name");
         let phone = document.getElementById("phone");
@@ -660,18 +357,11 @@ finalsubmitbutton.addEventListener('click',(event)=>{
                 }
             }
         });
-=======
-
-
-
-
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
 
 
         //SMTP request ends here
 
 
-<<<<<<< HEAD
         const api_url1 = "https://www.lazercrazer.in/api/date/bookslot"
         fetch(api_url1, {
             method: 'PUT',
@@ -690,36 +380,11 @@ finalsubmitbutton.addEventListener('click',(event)=>{
         }).then(function (data) {
             console.log(data);
         });
-=======
-
-
-
-        
-        const api_url1 ="http://localhost:5000/api/date/bookslot" 
-    fetch(api_url1, {
-        method: 'PUT',
-        body: JSON.stringify({
-            slotdate:todaysdate,
-            time:time,
-            slots:count
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-        }
-    }).then(function (response) {
-        if (response.ok) {
-            return response.json();
-        }
-    }).then(function (data) {
-        console.log(data);
-    });
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
 
 
 
 
 
-<<<<<<< HEAD
         const bookingsection = document.getElementsByClassName('bookingsection')[0];
         bookingsection.classList.remove('blurback')
         const popupsection = document.getElementsByClassName('popupsubsection')[0];
@@ -729,17 +394,6 @@ finalsubmitbutton.addEventListener('click',(event)=>{
         popupsection.style.transition = '0.5s';
         popupsection.style.zIndex = '-1';
 
-=======
-        const bookingsection=document.getElementsByClassName('bookingsection')[0];
-    bookingsection.classList.remove('blurback')
-    const popupsection=document.getElementsByClassName('popupsubsection')[0];
-    popupsection.style.top='40%';
-    popupsection.style.visibility='hidden';
-    popupsection.style.opacity='0';
-    popupsection.style.transition='0.5s';
-    popupsection.style.zIndex='-1';
-    
->>>>>>> 30c6b01901ad9f939a2066f82400eff08b221c54
     });
 
 }
